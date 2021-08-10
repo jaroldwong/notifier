c6ad18419818c6c30bc5bd8eafbb62baf00471fe
Jarold Wong <cwjwong@ucdavis.edu>
2021-08-10 16:53:50 -0700

    add loading state

--- src/components/NoticesList.js
+++ src/components/NoticesList.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core';
 import { capitalize } from '../utils';
+import apiClient from '../utils/apiClient';
 
+import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Chip from '@material-ui/core/Chip';
 import Container from '@material-ui/core/Container';
 import Table from '@material-ui/core/Table';
@@ -22,72 +25,91 @@ const NoticesList = () => {
   const classes = useStyles();
 
   const MS_IN_DAY = 86400000;
+  const [isLoading, setIsLoading] = useState(true);
   const [messages, setMessages] = useState([]);
   const [modifiers, setModifiers] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const messages = await fetch('/messages').then((response) =>
-        response.json()
-      );
-      const modifiers = await fetch('/modifiers').then((res) => res.json());
+      const messagesProm = apiClient.get('/messages');
+      const modifiersProm = apiClient.get('/modifiers');
 
-      setMessages(messages);
-      setModifiers(modifiers);
+      await Promise.all([messagesProm, modifiersProm]).then((results) => {
+        const [messages, modifiers] = results;
+        setMessages(messages);
+        setModifiers(modifiers);
+
+        setIsLoading(false);
+      });
     };
-
     fetchData();
   }, []);
 
   const handleModifier = () => {};
   return (
-    <Container>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Created</TableCell>
-              <TableCell>Subject</TableCell>
-              <TableCell>Author</TableCell>
-              <TableCell>Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {messages.map((d) => (
-              <TableRow key={`${d.created}-${d.subject}`}>
-                <TableCell>
-                  {new Intl.RelativeTimeFormat('en', {
-                    numeric: 'auto',
-                  }).format(
-                    Math.round((d.createdAt - Date.now()) / MS_IN_DAY),
-                    'day'
-                  )}
-                </TableCell>
-                <TableCell>{d.subject}</TableCell>
-                <TableCell>{d.author}</TableCell>
-                <TableCell style={{ width: 433 }}>
-                  {modifiers.map((m) => (
-                    <Chip
-                      className={classes.chip}
-                      label={capitalize(
-                        m.description.substring(0, m.description.indexOf(':'))
+    <>
+      {isLoading ? (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          margin="40px"
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Container>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Created</TableCell>
+                  <TableCell>Subject</TableCell>
+                  <TableCell>Author</TableCell>
+                  <TableCell>Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {messages.map((d) => (
+                  <TableRow key={`${d.created}-${d.subject}`}>
+                    <TableCell>
+                      {new Intl.RelativeTimeFormat('en', {
+                        numeric: 'auto',
+                      }).format(
+                        Math.round((d.createdAt - Date.now()) / MS_IN_DAY),
+                        'day'
                       )}
-                      variant="outlined"
-                    />
-                  ))}
-                  <Chip
-                    className={classes.chip}
-                    label="Archive"
-                    variant="outlined"
-                    onClick={handleModifier}
-                  />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Container>
+                    </TableCell>
+                    <TableCell>{d.subject}</TableCell>
+                    <TableCell>{d.author}</TableCell>
+                    <TableCell style={{ width: 433 }}>
+                      {modifiers.map((m) => (
+                        <Chip
+                          className={classes.chip}
+                          label={capitalize(
+                            m.description.substring(
+                              0,
+                              m.description.indexOf(':')
+                            )
+                          )}
+                          variant="outlined"
+                        />
+                      ))}
+                      <Chip
+                        className={classes.chip}
+                        label="Archive"
+                        variant="outlined"
+                        onClick={handleModifier}
+                      />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Container>
+      )}
+    </>
   );
 };
 
